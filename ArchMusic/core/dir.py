6850e7a02f5c52c821b7330109089b764031790c
Rq7mg <163336518+Rq7mg@users.noreply.github.com>
2024-06-23 06:36:25 +0300

    Update dir.py

--- ArchMusic/core/dir.py
+++ ArchMusic/core/dir.py
@@ -1,23 +1,13 @@
-#
-# Copyright (C) 2021-2023 by ArchBots@Github, < https://github.com/ArchBots >.
-#
-# This file is part of < https://github.com/ArchBots/ArchMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/ArchBots/ArchMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 import os
 import sys
 from os import listdir, mkdir
 
-from .logging import LOGGER
+from ..logging import LOGGER
 
 
 def dirr():
     if "assets" not in listdir():
-        LOGGER(__name__).warning(
+        LOGGER(name).warning(
             f"Assets Folder not Found. Please clone repository again."
         )
         sys.exit()
@@ -31,4 +21,4 @@ def dirr():
         mkdir("downloads")
     if "cache" not in listdir():
         mkdir("cache")
-    LOGGER(__name__).info("Directories Updated.")
+    LOGGER(name).info("Directories Updated.")
