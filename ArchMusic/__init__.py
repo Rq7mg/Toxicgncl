ff24b3158858a6de1a03dcda2894824990cf3cb5
Rq7mg <163336518+Rq7mg@users.noreply.github.com>
2024-06-23 06:38:03 +0300

    Update __init__.py

--- ArchMusic/__init__.py
+++ ArchMusic/__init__.py
@@ -1,13 +1,3 @@
-#
-# Copyright (C) 2021-2023 by ArchBots@Github, < https://github.com/ArchBots >.
-#
-# This file is part of < https://github.com/ArchBots/ArchMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/ArchBots/ArchMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from ArchMusic.core.bot import ArchMusic
 from ArchMusic.core.dir import dirr
 from ArchMusic.core.git import git
